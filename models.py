e25ac8c430a42e1336c7fe311ac3cb86f7186f0a
Craig Fouts <foutscw@gmail.com>
2023-11-08 13:03:07 -0500

    Fixed PyroLDA

--- models.py
+++ models.py
@@ -125,69 +125,6 @@ class GibbsLDA():
     def transform(self, _=None):
         doc_labels = self.doc_topic_dist_.argmax(-1)
         return doc_labels
-
-def normalize(arr):
-    return arr/arr.sum()
-
-def sample_categorical(probs):
-    n_bins = probs.shape[0]
-    num = np.random.rand(1)
-    sample = n_bins - 1
-    for i in range(n_bins - 1):
-        if num < probs[i]:
-            sample = i
-    return sample
-
-class GibbsGMM():
-    def __init__(self, n_topics, alpha=1., eta=1., sigma=1., init_random=True):
-        self.n_topics = n_topics
-        self.alpha = alpha
-        self.eta = eta
-        self.sigma = sigma
-        self.init_random = init_random
-
-        self.means_ = None
-        self.labels_ = None
-        self.counts_ = None
-        self.likelihood_log_ = []
-
-    def _build(self, n_samples, n_features):
-        if self.init_random:
-            self.means_ = np.random.normal(0., self.eta, size=(self.n_topics, n_features))
-        else:
-            self.means_ = np.zeros((self.n_topics, n_features))
-        self.labels_ = np.random.randint(self.n_topics, size=(n_samples,))
-        _, self.counts_ = np.unique(self.labels_, return_counts=True)
-        return self.means_, self.labels_, self.counts_
-
-    def _sample_topics(self, X):
-        for k in range(self.n_topics):
-            if self.counts_[k] == 0:
-                continue
-            topic_var = 1/(self.counts_[k]/self.sigma**2 + 1/self.eta**2)
-            idx = self.labels_ == k
-            topic_means = (self.counts_[k]/self.sigma**2)*topic_var*X[idx].mean(0)
-            self.means_[k] = np.random.normal(topic_means, topic_var)
-        return self.means_ 
-    
-    def _sample_words(self, X):
-        n_samples = X.shape[0]
-        self.likelihood_log_.append(0.)
-        for i in range(n_samples):
-            self.counts_[self.labels_[i]] -= 1
-            probs = normalize(1/((X[i] - self.means_)**2).sum(-1))
-            self.labels_[i] = sample_categorical(probs)
-            # self.labels_[i] = np.random.choice(self.n_topics, p=probs)
-            self.likelihood_log_[-1] += probs[self.labels_[i]].sum()
-            self.counts_[self.labels_[i]] += 1
-        return self.labels_
-
-    def fit(self, X, n_steps=100, verbose=True):
-        self._build(*X.shape)
-        for _ in tqdm(range(n_steps)) if verbose else range(n_steps):
-            self._sample_topics(X)
-            self._sample_words(X)
-        return self
     
 class PyroLDA():
     def __init__(self, n_topics, vocab_size, batch_size=150):
